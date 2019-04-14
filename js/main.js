d2e390c070a286481fd867662e298aba269d0db2
Shafiq Hammad <45017492+shafiqhammad@users.noreply.github.com>
2019-04-14 16:37:02 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,65 @@
+$(function() {
+    "use strict";
+
+    // Preloader 
+    $('body').addClass('preloader-active');
+    $(window).on('load', function() { 
+        $('.preloader').fadeOut();
+        $('.preloader-spinner').delay(350).fadeOut('slow');
+        $('body').removeClass('preloader-active');
+    });
+
+    //Countdown
+	$('#example').countdown({
+		date: '12/24/2020 23:59:59',
+		days: 'DAYS',
+		hours: 'HOURS',
+		minutes: 'MINS',
+		seconds: 'SECS'
+	});
+
+	//Scroll top 
+    $(".scroll-top").click(function(){
+        $("html,body").animate({scrollTop:0},1000);
+    });
+    $(window).scroll(function (){
+        if ($(this).scrollTop() > 180) { $(".scroll-top").fadeIn();}
+        else{
+            $(".scroll-top").fadeOut();
+        }
+    });
+    
+	function basicmap() {
+        // Basic options for a simple Google Map
+        // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
+        var mapOptions = {
+            // How zoomed in you want the map to start at (always required)
+            zoom: 11,
+            scrollwheel: false,
+            // The latitude and longitude to center the map (always required)
+            center: new google.maps.LatLng(43.438640, -80.559070), // New York
+            // This is where you would paste any style found on Snazzy Maps.
+            styles: [{ "featureType": "water", "elementType": "geometry", "stylers": [{ "color": "#e9e9e9" }, { "lightness": 17 }] }, { "featureType": "landscape", "elementType": "geometry", "stylers": [{ "color": "#f5f5f5" }, { "lightness": 20 }] }, { "featureType": "road.highway", "elementType": "geometry.fill", "stylers": [{ "color": "#ffffff" }, { "lightness": 17 }] }, { "featureType": "road.highway", "elementType": "geometry.stroke", "stylers": [{ "color": "#ffffff" }, { "lightness": 29 }, { "weight": .2 }] }, { "featureType": "road.arterial", "elementType": "geometry", "stylers": [{ "color": "#ffffff" }, { "lightness": 18 }] }, { "featureType": "road.local", "elementType": "geometry", "stylers": [{ "color": "#ffffff" }, { "lightness": 16 }] }, { "featureType": "poi", "elementType": "geometry", "stylers": [{ "color": "#f5f5f5" }, { "lightness": 21 }] }, { "featureType": "poi.park", "elementType": "geometry", "stylers": [{ "color": "#dedede" }, { "lightness": 21 }] }, { "elementType": "labels.text.stroke", "stylers": [{ "visibility": "on" }, { "color": "#ffffff" }, { "lightness": 16 }] }, { "elementType": "labels.text.fill", "stylers": [{ "saturation": 36 }, { "color": "#333333" }, { "lightness": 40 }] }, { "elementType": "labels.icon", "stylers": [{ "visibility": "off" }] }, { "featureType": "transit", "elementType": "geometry", "stylers": [{ "color": "#f2f2f2" }, { "lightness": 19 }] }, { "featureType": "administrative", "elementType": "geometry.fill", "stylers": [{ "color": "#fefefe" }, { "lightness": 20 }] }, { "featureType": "administrative", "elementType": "geometry.stroke", "stylers": [{ "color": "#fefefe" }, { "lightness": 17 }, { "weight": 1.2 }] }]
+        };
+        // Get the HTML DOM element that will contain your map 
+        // We are using a div with id="map" seen below in the <body>
+        var mapElement = document.getElementById('contact-map');
+
+        // Create the Google Map using our element and options defined above
+        var map = new google.maps.Map(mapElement, mapOptions);
+
+        // Let's also add a marker while we're at it
+        var marker = new google.maps.Marker({
+            position: new google.maps.LatLng(43.438640, -80.559070),
+            map: map,
+            title: ' 221b Baker Street - London - United Kingdom'
+        });
+    }
+    if ($('#contact-map').length != 0) {
+        google.maps.event.addDomListener(window, 'load', basicmap);
+    };
+
+
+
+    
+});(jQuery)
